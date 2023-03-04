5674bfdbb4c42d6569aade1c2c7a09d65bc628b9
Mustafa-isa <116859259+Mustafa-isa@users.noreply.github.com>
2023-03-04 11:52:51 +0200

    .

--- index.js
+++ index.js
@@ -1,54 +1,48 @@
 //start bring data
 let data = [
-    {
-        product_img:"./images/giorgio-trovato-K62u25Jk6vo-unsplash.jpg",
-        product_name:"trovato",
-       product_price:1241,
-       added_to_cart : false
-
-    },
-    {
-        product_img:"./images/giorgio-trovato-p0OlRAAYXLY-unsplash.jpg",
-        product_name:"unsplash",
-       product_price:43,
-       added_to_cart : false
-
-    },
-    {
-        product_img:"./images/grant-ritchie-n_wXNttWVGs-unsplash.jpg",
-        product_name:"richie",
-       product_price:121,
-       added_to_cart : false
-
-    },
-    {
-        product_img:"./images/kiran-ck-LSNJ-pltdu8-unsplash.jpg",
-        product_name:"kiran",
-       product_price:34,
-       added_to_cart : false
-
-    },
-    {
-        product_img:"./images/olena-sergienko-ElfJDs4LAQk-unsplash.jpg",
-        product_name:"olena",
-       product_price:11,
-       added_to_cart : false
-
-    },
-    {
-        product_img:"./images/ruslan-bardash-4kTbAMRAHtQ-unsplash.jpg",
-        product_name:"ruslan",
-       product_price:121,
-       added_to_cart : false
-
-    }
-]
+  {
+    product_img: "./images/giorgio-trovato-K62u25Jk6vo-unsplash.jpg",
+    product_name: "trovato",
+    product_price: 1241,
+    added_to_cart: false,
+  },
+  {
+    product_img: "./images/giorgio-trovato-p0OlRAAYXLY-unsplash.jpg",
+    product_name: "unsplash",
+    product_price: 43,
+    added_to_cart: false,
+  },
+  {
+    product_img: "./images/grant-ritchie-n_wXNttWVGs-unsplash.jpg",
+    product_name: "richie",
+    product_price: 121,
+    added_to_cart: false,
+  },
+  {
+    product_img: "./images/kiran-ck-LSNJ-pltdu8-unsplash.jpg",
+    product_name: "kiran",
+    product_price: 34,
+    added_to_cart: false,
+  },
+  {
+    product_img: "./images/olena-sergienko-ElfJDs4LAQk-unsplash.jpg",
+    product_name: "olena",
+    product_price: 11,
+    added_to_cart: false,
+  },
+  {
+    product_img: "./images/ruslan-bardash-4kTbAMRAHtQ-unsplash.jpg",
+    product_name: "ruslan",
+    product_price: 121,
+    added_to_cart: false,
+  },
+];
+let slideArr =[]
 //render element in page
 
-
-const container =document.querySelector(".container")
-data.forEach(function (el){
-    const item = `
+const container = document.querySelector(".container");
+data.forEach(function (el) {
+  const item = `
       
     <div class="product">
     <div class="imgContainer">
@@ -60,72 +54,110 @@ data.forEach(function (el){
      <span class="price">${el.product_price} <i class="fa-solid fa-dollar-sign"></i></span>
 
      <div class="buttons">
-         <button id="add" condation =${el.added_to_cart}> add</button>
+         <button id="add" class ="add" add__to_cart =${el.added_to_cart} > add</button>
  <button class="show" >show</button>
     </div>
  </div>
  </div>
     
-    `
-    
-container.innerHTML += item
-})
+    `;
+
+  container.innerHTML += item;
+});
 //loop data
-function showElement(){
-
-    let showBtn = document.querySelectorAll('.show')
-    console.log(showBtn)
-   showBtn.forEach(btn =>{
-
-btn.addEventListener('click' ,function(){
-   let productCloned =  btn.closest(".product")
-addModul(productCloned)
-})
-})
-
+function showElement() {
+  let showBtn = document.querySelectorAll(".show");
+  console.log(showBtn);
+  showBtn.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      let productCloned = btn.closest(".product");
+      addModul(productCloned);
+    });
+  });
 }
 //show element in a moduul
-function addModul(elementShowed){
+function addModul(elementShowed) {
+  let modul = document.querySelector(".modul");
+  window.location.href = "#modul";
+  let modulcontent = document.getElementById("modulcontent");
+  modul.classList.add("show");
+  let sourceImg = elementShowed.querySelector("img").src;
+  let name = elementShowed.querySelector(".name").innerText;
+  let price = elementShowed.querySelector(".price").innerText;
+  console.log(price, name);
 
-
-    let modul = document.querySelector(".modul")
-    window.location.href ="#modul"
-    let modulcontent = document.getElementById("modulcontent")
-modul.classList.add("show")
-let sourceImg = elementShowed.querySelector("img").src
-let name  = elementShowed.querySelector(".name").innerText
-let price = elementShowed.querySelector(".price").innerText
-console.log(price ,name)
-
-modulcontent.innerHTML = `
+  modulcontent.innerHTML = `
 <img src="${sourceImg}" />
 <h2>${name}</h2>
 <p>${price} <i class="fa-solid fa-dollar-sign"></i></p>
 <button class="deleteFromMODUL"><i class="fa-solid fa-xmark"></i><ibutton>
 
-`
-let deleteFromMODUL =document.querySelector(".deleteFromMODUL")
-deleteFromMODUL.addEventListener('click' ,function(){
-
-    modul.classList.remove("show")
-})
+`;
+  let deleteFromMODUL = document.querySelector(".deleteFromMODUL");
+  deleteFromMODUL.addEventListener("click", function () {
+    modul.classList.remove("show");
+  });
 }
-showElement()
+showElement();
 //show  down side
-function showSide(){
-let minu =document.querySelector(".dropdown")
-let cart = document.querySelector(".cart")
-let hideBtn =  document.querySelector(".deleteFromMODUL")
-function downSide(){
+function showSide() {
+  let minu = document.querySelector(".dropdown");
+  let cart = document.querySelector(".cart");
+  let hideBtn = document.querySelector(".deleteFromMODUL");
+  function downSide() {
+    minu.classList.toggle("shown");
+  }
+  function hide() {
+    minu.classList.remove("shown");
+  }
+  cart.addEventListener("click", downSide);
+  hideBtn.addEventListener("click", hide);
+}
+//
+showSide();
+///add product to cart
+const addCart = function () {
+  let addBtn = document.querySelectorAll(".add");
+  addBtn.forEach((btn) => {
 
-    minu.classList.add("shown")
-}
-function hide(){
-    minu.classList.remove("shown")
+    btn.addEventListener("click", function () {
+        let unic =Math.random()
+        let id
+      if (btn.getAttribute("add__to_cart") === "false") {
+        console.log("add");
+        btn.setAttribute("add__to_cart", true)
+        btn.innerText ='remove'
+        let productAdded = btn.closest(".product");
 
-}
-cart.addEventListener('click' ,downSide)
-hideBtn.addEventListener('click' , hide)
-}
-// 
-showSide()
+ let sourceImgAdded = productAdded.querySelector("img").src;
+  let nameAdded = productAdded.querySelector(".name").innerText;
+  let priceAdded = productAdded.querySelector(".price").innerText;
+
+  let product ={
+
+    name:nameAdded,
+    price:priceAdded,
+    img:sourceImgAdded,
+    id :unic
+  }
+  id =product.id
+
+  slideArr.push(product)
+  console.log(slideArr)
+      } else {
+        console.log("remove");
+        btn.innerText ='add'
+        btn.setAttribute("add__to_cart", false)
+
+        slideArr =slideArr.filter(el => {
+            
+            
+          return  el.id ==id ? "" :el
+        
+        })
+      }
+    });
+  });
+};
+//
+addCart();
